ad827a763fe2ef146d6dead3541a5bbfa7e3d1af
Nzubeee_e <67470583+Nikeemma12@users.noreply.github.com>
2025-07-11 11:34:21 +0100

    Add files via upload

---
+++ hang.js
@@ -0,0 +1,149 @@
+const head = document.getElementById("head")
+const body = document.getElementById("body")
+const rhand = document.getElementById("right-hand")
+const lhand = document.getElementById("left-hand")
+const rleg = document.getElementById("right-leg")
+const lleg = document.getElementById("left-leg")
+let word = document.getElementById("word")
+let score = document.getElementById("score")
+const container = document.querySelector(".body-container")
+const score_container = document.querySelector(".score")
+const reset = document.getElementById("reset")
+const general_container = document.querySelector(".general")
+const mode = document.querySelector(".mode")
+const diff = document.getElementById("diff")
+
+let guess = 0
+const parts = [head, body, rhand, lhand, rleg, lleg]
+let guessWords = [
+    ["cat","sun","tree","game","ball","book","fish","bird","moon","rain"],
+    ["window","blanket","thunder","kitchen","monster","desert","laptop","school","garden","jellyfish"],
+    ["internship","microscope","adventure","dictionary","spaceship","earthquake","backpacker","waterfall","basketball","holograph"],
+]
+let guessWord
+let guessChoice
+let randomNo
+const start = (difficulty) => {
+    mode.style.display = 'none'
+    diff.style.display = 'none'
+    general_container.style.display = 'block'
+    let indexes
+    if(difficulty === "EASY"){
+        guessChoice = guessWords[0]
+        randomNo = Math.floor(Math.random()*guessChoice.length)
+        guessWord = guessChoice[randomNo]
+    } else if(difficulty === "MEDIUM") {
+        guessChoice = guessWords[1]
+        randomNo = Math.floor(Math.random()*guessChoice.length)
+        guessWord = guessChoice[randomNo]
+    } else if(difficulty === "HARD") {
+        guessChoice = guessWords[2]
+        randomNo = Math.floor(Math.random()*guessChoice.length)
+        guessWord = guessChoice[randomNo]
+    }
+    console.log(guessWord)
+    game(guessWord)
+    document.addEventListener(
+        "keydown",
+        (event) => {
+            let letter = event.key.toLocaleLowerCase()
+            console.log(letter)
+            if(guessWord.includes(letter)) {
+                let index = guessWord.indexOf(letter)
+                let final = guessWord.lastIndexOf(letter)
+                word.innerText = word.innerText.slice(0,index) + letter + word.innerText.slice(index+1)
+                word.innerText = word.innerText.slice(0,final) + letter + word.innerText.slice(final+1)
+                
+            } else {
+                if(guess<parts.length) {
+                    parts[guess].style.display = 'block'
+                    guess++
+                    if(guess == parts.length) {
+                        score.innerText = `You failed to guess the word. The word is "${guessWord}"`
+                        container.style.display = "none"
+                        word.style.display = 'none'
+                        score_container.style.display = "block"
+                        score_container.style.margin = "100px"
+                        score.style.backgroundColor = "blueviolet"
+                        reset.style.display = "block"
+                    }
+                }
+                
+            }
+            if(word.innerText == guessWord) {
+                score.innerText = "Your guessed the word correctly"
+                container.style.display = "none"
+                word.style.display = 'none'
+                score_container.style.display = "block"
+                score_container.style.margin = "100px"
+                score.style.backgroundColor = "blueviolet"
+                reset.style.display = "block"
+            }
+            
+        } 
+    )
+}
+let word_index = []
+let indexNo
+const game = (a) => {
+    for(letter in a){
+        word.innerText +=  '-'
+    }
+    if(a.length>5 && a.length<10){
+        indexes = 2
+        console.log(select(indexes,a))
+        if(word_index[0]===word_index[1]){
+            console.log(select(indexes,a))
+        }
+        word.innerText = word.innerText.slice(0,word_index[0]) + guessWord[word_index[0]] + word.innerText.slice(word_index[0]+1)
+        word.innerText = word.innerText.slice(0,word_index[1]) + guessWord[word_index[1]] + word.innerText.slice(word_index[1]+1)
+    } else if(a.length>=10) {
+        indexes = 3
+        console.log(select(indexes,a))
+        if(word_index[0]===word_index[1]){
+            console.log(select(indexes,a))
+        }
+        word.innerText = word.innerText.slice(0,word_index[0]) + guessWord[word_index[0]] + word.innerText.slice(word_index[0]+1)
+        word.innerText = word.innerText.slice(0,word_index[1]) + guessWord[word_index[1]] + word.innerText.slice(word_index[1]+1)
+        word.innerText = word.innerText.slice(0,word_index[2]) + guessWord[word_index[2]] + word.innerText.slice(word_index[2]+1)
+    }
+    else {
+        indexes = 1
+        console.log(select(indexes,a))
+        word.innerText = word.innerText.slice(0,word_index[0]) + guessWord[word_index[0]] + word.innerText.slice(word_index[0]+1)
+    }
+    
+}
+const select = (indexes,a) => {
+    for(let i=0; i<indexes;i++){
+        indexNo = Math.floor(Math.random()*a.length)
+        word_index.push(indexNo)
+    }
+    word_index.sort((a,b) => a-b)
+    return word_index
+}
+const playagain = () => {
+    location.reload()
+}
+const hint_word = document.getElementById("hint")
+async function hint(element) {
+    try {
+        guessWord  = guessWord.toLocaleLowerCase()
+        const word = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${guessWord}`)   
+        if(!word.ok) {
+            throw new Error("Could not fetch data")
+        }
+        const data = await word.json()
+        console.log(data)
+        console.log(data.length)
+        let dataindex = Math.floor(Math.random()*data.length)
+        let meanings = data[dataindex].meanings
+        let meaningsindex = Math.floor(Math.random()*meanings.length)
+        let definitions = meanings[meaningsindex].definitions
+        let definitionsindex = Math.floor(Math.random()*definitions.length)
+        let definition = definitions[definitionsindex].definition
+        hint_word.innerText = definition
+    } catch (error) {
+        console.log(error + " Could not fetch data Make sure your connected to internet")
+    }
+}
